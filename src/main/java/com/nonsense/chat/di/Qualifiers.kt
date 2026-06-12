8b2939e4cf3e993c2caf4bb23e06adc5dbeb8a1b
cifrakitiket <g0tr00tkitiket@gmail.com>
2026-06-12 21:34:28 +0500

    Delete src/main directory

--- src/main/java/com/nonsense/chat/di/Qualifiers.kt
+++
@@ -1,8 +0,0 @@
-package com.nonsense.chat.di
-
-import javax.inject.Qualifier
-
-/** Application-lifetime coroutine scope (survives individual screens/VMs). */
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class AppScope
